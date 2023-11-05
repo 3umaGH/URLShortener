6cb352cb6cc14bdc65ec77fe276dfc54a3590268
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-05 15:50:27 +0200

    init commit, dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h2">Hello World!</Typography>
+    </Container>
+  );
+}
+
+export default App;
