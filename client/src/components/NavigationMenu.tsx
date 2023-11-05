84c0ee384003492580d24a8853d976d306a9a22c
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-05 22:06:34 +0200

    fix: fixed margin

--- client/src/components/NavigationMenu.tsx
+++ client/src/components/NavigationMenu.tsx
@@ -1,7 +1,35 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
 import { Box } from "@mui/material";
+import { NavigationButton } from "./NavigationButton";
 
 export const NavigationMenu = () => {
   return (
-    <Box></Box>
-  )
-}
+    <Container disableGutters sx={{ mt: -1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          m: 0,
+          backgroundColor: "rgba(0,0,0,0.0)",
+          color: "#4a4a4a",
+          boxShadow: "none",
+        }}
+      >
+        <Toolbar sx={{display:"flex", justifyContent: "space-evenly"}}>
+          <Box sx={{ maxWidth: "5%", minWidth: "120px" }}>
+            <img
+              src="https://images.squarespace-cdn.com/content/v1/5cd90f9a81551228736aaa01/1559565680227-38K8XVENP2S8KOR7GP2Z/placeholder.com-logo1.png?format=2500w"
+              alt="Logo"
+              width="100%"
+            ></img>
+          </Box>
+          <NavigationButton title="Products" path="/" />
+          <NavigationButton title="View Analytics" path="/" />
+
+          
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
+};
