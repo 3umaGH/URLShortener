00a0be8b3429039ed6a96184c7e0684a8d5307d8
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-08 15:19:08 +0200

    feat: implemented data grid, page navigation in LinkForm

--- client/src/components/LinkForm/pages/Analytics.tsx
+++ client/src/components/LinkForm/pages/Analytics.tsx
@@ -1,17 +1,15 @@
-import {
-  Button,
-  Grid,
-  InputLabel,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Button, Grid, TextField, InputLabel, Typography } from "@mui/material";
 
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 import { Tip } from "../../Tip";
 import { AnalyticsGrid } from "../../AnalyticsGrid";
 
-export const Analytics = () => {
+export const Analytics = ({
+  navigatePage,
+}: {
+  navigatePage: (pageID: number) => void;
+}) => {
   return (
     <>
       <Grid item xs={12} sx={{ textAlign: "left" }}>
@@ -21,35 +19,11 @@ export const Analytics = () => {
       </Grid>
 
       <Grid item xs={12} sx={{ textAlign: "center" }}>
-      <AnalyticsGrid />
-        {/*<List
-          sx={{
-            width: "100%",
-            bgcolor: "background.paper",
-          }}
-        >
-          {[1, 2, 3].map((num) => {
-            return (
-              <ListItemButton key={num} disableGutters>
-                <Typography variant="subtitle1" sx={{ width: "100%" }}>
-                  <ListItemText
-                    primary="https://short.ly/my-portfolio-link"
-                    secondary="https://somelonglink.com/portfolio/id/14/523534/public"
-                    sx={{
-                      wordBreak: "break-all",
-                      textAlign: "center",
-                    }}
-                  />
-                  <Divider />
-                </Typography>
-              </ListItemButton>
-            );
-          })}
-        </List>*/}
+        <AnalyticsGrid />
       </Grid>
 
       <Grid item xs={12} sx={{ textAlign: "left" }}>
-        <InputLabel htmlFor={"secret-key-field"} sx={{ mb: 1 }}>
+        <InputLabel htmlFor={"secret-key-field"} sx={{ mb: -1 }}>
           <b>Paste secret key</b>{" "}
           <Tip text="If someone has provided you with their private link key, you can import it here to access link analytics.">
             <InfoOutlinedIcon
@@ -61,22 +35,24 @@ export const Analytics = () => {
             />
           </Tip>
         </InputLabel>
-        <Grid item xs={12} sx={{ textAlign: "left" }}>
+      </Grid>
 
-          <TextField
-            id="secret-key-field"
-            placeholder="Example: 550e8400-e29b-41d4-a716-446655440000"
-            variant="outlined"
-            sx={{width:"80%"}}
-            />
-          <Button
-            variant="contained"
-            sx={{ width: "19%", p: 1.95, ml: 1 }}
-            color="secondary"
-          >
-            Add Link
-          </Button>
-        </Grid>
+      <Grid item xs={12} sm={10} sx={{ textAlign: "left" }}>
+        <TextField
+          id="secret-key-field"
+          placeholder="Example: 550e8400-e29b-41d4-a716-446655440000"
+          variant="outlined"
+          sx={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid item xs={12} sm={2} sx={{ textAlign: "left" }}>
+        <Button
+          variant="contained"
+          sx={{ width: "100%", p: 1.95 }}
+          color="secondary"
+        >
+          Add Link
+        </Button>
       </Grid>
     </>
   );
