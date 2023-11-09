3543fdae108ccc0242c29344a6f65351c397b684
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-09 15:28:20 +0200

    feat: MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
+// DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+
+//const uri = `mongodb://root:abvUdGuj46Xn78CD@95.179.187.231:27017/ShortURL`;
+
+// Connect to MongoDB
+const connect = () => {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+// Listen for the connection event
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected");
+});
+
+// Listen for the error event
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connction error: " + err);
+});
+
+// Listen for the disconnection event
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected trying to reconnect.");
+
+  setTimeout(() => {
+    mongoose.connect(uri);
+  }, 1000);
+});
+
+connect();
 
 app.use("/action", require("./routes/Action"));
 app.use("/", require("./routes/ShortenedURL"));
