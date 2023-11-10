d8af4c97b06a14ae1ecd4ac74768415b7c75e5da
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-10 17:40:17 +0200

    feat: analytics gets fetched from database

--- client/src/components/LinkForm/pages/PageStats.tsx
+++ client/src/components/LinkForm/pages/PageStats.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { Button, Grid, Typography, Tooltip } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,172 +10,165 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { PayloadType } from "../LinkForm";
+import axios, { AxiosResponse } from "axios";
 
-export const PageStats = () => {
-  const linkClicks = 16;
+type Referral = {
+  referralUrl: string;
+  count: number;
+};
+
+type Country = {
+  country: string;
+  count: number;
+};
+
+type URLDetailsType = {
+  id: string;
+  originalURL: string;
+  URLSuffix: string;
+  totalClicks: number;
+  referrals: Referral[];
+  countries: Country[];
+  dateCreated: string;
+};
+
+export const PageStats = ({ payload = {} }: { payload?: PayloadType }) => {
+  const [pageStatistics, setPageStatistics] = useState<URLDetailsType>();
+  const { uuid } = payload ?? {};
+
+  useEffect(() => {
+    const fetch = async () => {
+      axios
+        .get(`${import.meta.env.VITE_APP_API}/action/${uuid}`)
+        .then((response: AxiosResponse) => {
+          const apiResponse: URLDetailsType = response.data.shortLink;
+
+          if (apiResponse && response.data.message === "OK") {
+            setPageStatistics(apiResponse);
+          } else {
+            //TODO: Error handling
+          }
+        });
+    };
+
+    if (!pageStatistics) fetch();
+  }, [pageStatistics, uuid]);
 
   return (
     <>
-      <Grid item xs={12} sx={{ textAlign: "left" }}>
-        <Typography variant="h4">
-          <b>Link Statistics</b>
-        </Typography>
-      </Grid>
+      {pageStatistics && (
+        <>
+          <Grid item xs={12} sx={{ textAlign: "left" }}>
+            <Typography variant="h4">
+              <b>Link Statistics</b>
+            </Typography>
+          </Grid>
 
-      <Grid item xs={12} sx={{ my: 2 }}>
-        <Typography variant="h6">Total Link Clicks:</Typography>
-        <Typography
-          className="analytics-total-link-clicks-label"
-          variant="h3"
-          sx={{ display: "inline-block", py: 2, px: 2.5, mt: 1 }}
-        >
-          {linkClicks}
-        </Typography>
-      </Grid>
+          <Grid item xs={12} sx={{ my: 2 }}>
+            <Typography variant="h6">Total Link Clicks:</Typography>
+            <Typography
+              className="analytics-total-link-clicks-label"
+              variant="h3"
+              sx={{ display: "inline-block", py: 2, px: 2.5, mt: 1 }}
+            >
+              {pageStatistics.totalClicks}
+            </Typography>
+          </Grid>
 
-      <Grid item xs={12} md={6}>
-        <Typography variant="h6" style={{ display: "inline-block" }}>
-          Top Refferals
-        </Typography>
-        <Tooltip
-          arrow
-          enterTouchDelay={0}
-          title="Insight into Sources: Discover where clicks originate, guiding marketing strategies towards effective channels."
-        >
-          <InfoOutlinedIcon
-            fontSize="small"
-            sx={{
-              color: "gray",
-              ml: 0.5,
-            }}
-          />
-        </Tooltip>
-        <TableContainer component={Paper} sx={{ mt: 1 }}>
-          <Table sx={{ minWidth: "100%" }} aria-label="refferals">
-            <TableHead>
-              <TableRow>
-                <TableCell sx={{ width: "75%" }} align="left">
-                  Refferal
-                </TableCell>
-                <TableCell sx={{ width: "15%" }} align="center">
-                  Clicks
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell align="left">
-                  https://www.youtube.com/watch?v=dQw4w9WgXcQ
-                </TableCell>
-                <TableCell align="center">
-                  <b>158</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">
-                  https://en.wikipedia.org/wiki/Philosophy
-                </TableCell>
-                <TableCell align="center">
-                  <b>18</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">
-                  https://m.youtube.com/watch?v=jQ41Gk80djs
-                </TableCell>
-                <TableCell align="center">
-                  <b>8</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">about:info</TableCell>
-                <TableCell align="center">
-                  <b>3</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">
-                  https://www.unicef.org/education
-                </TableCell>
-                <TableCell align="center">
-                  <b>1</b>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" style={{ display: "inline-block" }}>
+              Top Refferals
+            </Typography>
+            <Tooltip
+              arrow
+              enterTouchDelay={0}
+              title="Insight into Sources: Discover where clicks originate, guiding marketing strategies towards effective channels."
+            >
+              <InfoOutlinedIcon
+                fontSize="small"
+                sx={{
+                  color: "gray",
+                  ml: 0.5,
+                }}
+              />
+            </Tooltip>
+            <TableContainer component={Paper} sx={{ mt: 1 }}>
+              <Table sx={{ minWidth: "100%" }} aria-label="refferals">
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ width: "75%" }} align="left">
+                      Refferal
+                    </TableCell>
+                    <TableCell sx={{ width: "15%" }} align="center">
+                      Clicks
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {pageStatistics.referrals.map((refer) => (
+                    <TableRow key={refer.referralUrl}>
+                      <TableCell align="left">{refer.referralUrl}</TableCell>
+                      <TableCell align="center">
+                        <b>{refer.count}</b>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
 
-      <Grid item xs={12} md={6}>
-        <Typography variant="h6" style={{ display: "inline-block" }}>
-          Top Countries
-        </Typography>
-        <Tooltip
-          arrow
-          enterTouchDelay={0}
-          title="Geographic Reach: Understand user locations for tailored content and targeted campaigns."
-        >
-          <InfoOutlinedIcon
-            fontSize="small"
-            sx={{
-              color: "gray",
-              ml: 0.5,
-            }}
-          />
-        </Tooltip>
-        <TableContainer component={Paper} sx={{ mt: 1 }}>
-          <Table sx={{ minWidth: "100%" }} aria-label="refferals">
-            <TableHead>
-              <TableRow>
-                <TableCell sx={{ width: "75%" }} align="left">
-                  Contry
-                </TableCell>
-                <TableCell sx={{ width: "15%" }} align="center">
-                  Clicks
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell align="left">USA</TableCell>
-                <TableCell align="center">
-                  <b>158</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">England</TableCell>
-                <TableCell align="center">
-                  <b>18</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">Australia</TableCell>
-                <TableCell align="center">
-                  <b>8</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">Estonia</TableCell>
-                <TableCell align="center">
-                  <b>3</b>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left">Zimbabwe</TableCell>
-                <TableCell align="center">
-                  <b>1</b>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" style={{ display: "inline-block" }}>
+              Top Countries
+            </Typography>
+            <Tooltip
+              arrow
+              enterTouchDelay={0}
+              title="Geographic Reach: Understand user locations for tailored content and targeted campaigns."
+            >
+              <InfoOutlinedIcon
+                fontSize="small"
+                sx={{
+                  color: "gray",
+                  ml: 0.5,
+                }}
+              />
+            </Tooltip>
+            <TableContainer component={Paper} sx={{ mt: 1 }}>
+              <Table sx={{ minWidth: "100%" }} aria-label="refferals">
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ width: "75%" }} align="left">
+                      Contry
+                    </TableCell>
+                    <TableCell sx={{ width: "15%" }} align="center">
+                      Clicks
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {pageStatistics.countries.map((country) => (
+                    <TableRow key={country.country}>
+                      <TableCell align="left">{country.country}</TableCell>
+                      <TableCell align="center">
+                        <b>{country.count}</b>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
 
-      <Grid item xs={12}>
-        <Button variant="contained" color="error" sx={{ mt: 1 }}>
-          Delete Link
-        </Button>
-      </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="error" sx={{ mt: 1 }}>
+              Delete Link
+            </Button>
+          </Grid>
+        </>
+      )}
     </>
   );
 };
